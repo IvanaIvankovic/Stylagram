b6e948b903e07db2251e63a5bef1e64abee80799
Ivana <ivana93.mail@gmail.com>
2019-10-30 12:47:29 +0100

    Profile Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Profile />
     </div>
   );
 }
